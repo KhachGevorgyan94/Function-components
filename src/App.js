6103fdcef5dd0f874f37b26246041352dd29ceed
khach <khachgevorgyan94@gmail.com>
2023-09-30 12:36:45 +0400

    new components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,75 @@
 import './App.css';
-import React, {useState} from "react";
-import {Modal} from "./components/modal";
-import {LoginForm} from "./components/login-form";
-import {Header} from "./components/header";
+import React, {useEffect, useId, useState} from "react";
+import {Counter} from "./components/counter";
 
 function App() {
-    const [isOpen, setIsModal] = useState(false)
-    const [isOpenModalLogin, setIsOpenModalLogin] = useState(false)
+    const [counter, setCounter] = useState(0)
+    let x = useId()
+    console.log('11111111111111111111111111111')
+    const [hideButton, setHideButton] = useState(false)
+
+    let intervalData = null
+
+
+    useEffect(() => {
+        console.log(x, 'state 1')
+        console.log('222222222222222222222222222')
+    }, [])
+
+    const handleCounter = () => {
+        setCounter(counter + 1)
+        console.log(counter, 'counter')
+    }
+
+
+    useEffect(() => {
+        console.log(counter, 'counter useEffect')
+        console.log('55555555555555555555555')
+
+    }, [counter])
+
+    console.log('3333333333333333333333333333333333')
+
+
+    useEffect(() => {
+
+
+        return () => {
+
+        }
+
+    }, []);
+
 
     return <div>
-        <Header/>
-        <button onClick={() => setIsModal(true)}>Open modal</button>
-        <button onClick={() => setIsOpenModalLogin(true)}>Open modal</button>
+        {console.log('44444444444444444444444444')}
+        <button onClick={handleCounter}>click me</button>
+        <button onClick={()=>setHideButton(!hideButton)}>hide button</button>
+        <h1>{counter}</h1>
 
-        {isOpen ? <Modal
-            onClose={() => setIsModal(false)}
-            title={'Google map'}>
-            <button onClick={() => setIsModal(false)}>Cancle</button>
-
-        </Modal> : null}
-        {isOpenModalLogin ? <Modal
-            title={'Log in'}>
-            <LoginForm onClose={() => setIsOpenModalLogin(false)}/>
-        </Modal> : null}
+        {!hideButton ? <Counter/> : null}
     </div>
 }
 
 export default App
 
+
+// class App extends React.Component {
+//     componentDidMount() {
+//         {console.log('asdfasdfasdfasdf')}
+//
+//     }
+//     componentDidUpdate(prevProps, prevState, snapshot) {
+//
+//     }
+//
+//     componentWillUnmount() {
+//
+//     }
+//
+//     render() {
+//         return <div>ASDFASD  </div>
+//     }
+// }
+//
+// export default App
