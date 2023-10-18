0354573af7f661cd803c0a3436dfaa4f25d222e5
khach <khachgevorgyan94@gmail.com>
2023-10-18 10:47:20 +0400

    redux  lesson

--- src/store/reducers/products/index.js
+++ src/store/reducers/products/index.js
@@ -46,3 +46,68 @@ export const productReducer = (state = initialState, action)=>{
     }
 
 }
+
+
+
+/**
+ * Tnain arajadrnanqn  hetevyaln e
+ *
+ * Stanal 3 ej
+ *      1. User registration
+ *      2. Login user
+ *      3. User information
+ *
+ *  xndir@ kirarelu hamar  ogtagorcum eq
+ *  raact router dom routingi hamar
+ *  react redux state management  pahpanelu hamar
+ *
+ *  @UserRegistration(page):
+ *     unenalu eq forma  vori mijocov stexcelu eq  userneri canq
+ *     hetevyal dashterov
+ *     1. Anun,
+ *     2. Azganun,
+ *     3. Position,
+ *     4. Email,
+ *     5. Password
+ *     6. Confirm password
+ *
+ *     grel validacia hetevyal dashteri  hamar stugel
+ *     1. bolor dashter@ lracvac e  te  voch
+ *     2. stugel password u confirm password dashter@ ham@Nknum en ira r
+ *     3. stugel  nman email artahaytutyun goyutyun uni te che
+ *
+ *     buttoni vra click aneluc  ayd  informacian arden reduzi mijocov havaqel  stori mej
+ *
+ *  @UserLogin(page):
+ *     ays  ejum unenalu eq  2 input dasht  ev button
+ *     input dashtern en `
+ *     1. email
+ *     2. password
+ *
+ *     xndir@ hetevyaln e ayd ejum  email  password greluc , buttoni vra click aneluc
+ *     stugum eq hetevyal validacian
+ *
+ *     STUGEL NMAN MARD  MER AVELACVAC CUCAKUM KA TE CHE
+ *     ete chka cuyc tal input dashter@ karmir guyni
+ *     hakarak depqum navigate anel  nor ej
+ *
+ *
+ *     @UserDetails(page):
+ *
+ *     ays  ej@  ashxatelu e miayn login lineluc heto
+ *     aysinqn  sovorakan ete meqn uzenanaq  linkn  brouzerum dnenq ayd ej@ chpetqa bacvi
+ *
+ *
+ *     @ndhanut  xndir npatakn e hetevyal qayler@
+ *
+ *     1.  Stexcel  userner
+ *     2. login linelu jamanak stugel nman  user  ka te che
+ *     3. ete nman user gtnvec apa  navigate anel  user details ej
+ *     4. aranc  login linelu chpetqa hasaneli linel user details ej@
+ *     5. apahovel bolor input dashteri hamar ver@ nshvac validacianer@
+ *
+ *
+ *
+ *
+ *
+ * **/
